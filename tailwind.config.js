82f2092a92d1181b34be8742b90ebd7d796b3fb5
caowen <caowen@mail.taiji.com.cn>
2021-01-18 17:01:43 +0800

    修改样式

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,31 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      minHeight: {
+        '50': '50rem',
+       },
+      backgroundImage: theme => ({
+        'hero-pattern': "url('/src/bg.png')",
+        'delete': "url('/src/delete.png')",
+        'save': "url('/src/save.png')",
+       }),
+       backgroundSize: {
+        '100': '100% auto',
+        '15': '1.5rem auto',
+        '16': '1.6rem auto',
+      },
+      fontFamily: {
+        'myh': 'Microsoft YaHei',
+       }
+
+
+
+
+
+
+
+
+
     },
   },
   variants: {
